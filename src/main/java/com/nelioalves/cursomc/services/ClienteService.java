4b941bd5e8c7d14f076ccacc3ff3ca16821dd553
Nathalie Kessy <nathalienkm@gmail.com>
2026-07-28 16:14:03 -0300

    Endpoint /cliente{id} disponivel

---
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	private final ClienteRepository repo;
+
+	ClienteService(ClienteRepository repo) {
+		this.repo = repo;
+	}
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
